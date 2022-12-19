4fe9d218f40c246147596925356e25b000f60d08
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-19 22:23:31 +0530

    card-list updated to ts

---
+++ src/components/card-list/card-list.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Monster } from "../../App";
+import { Card } from "../card/Card";
+
+import "./card-list.css";
+
+type cardListProps = {
+  monsters: Monster[];
+};
+
+const CardList = ({ monsters }: cardListProps) => (
+  <div className='card-list'>
+    {monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
+export default CardList;
