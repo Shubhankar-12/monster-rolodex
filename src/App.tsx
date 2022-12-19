78bedc5a70c6a334c245816f6571814a4fc5e7ef
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-19 22:16:22 +0530

    app updated to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import CardList from "./components/card-list/card-list";
 import SearchBox from "./components/search-box/SearchBox";
 import "./App.css";
+import { getData } from "./utils/data.utils";
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
 
 const App = () => {
   console.log("render");
   const [searchField, setSearchField] = useState("");
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setMonsters(users);
+    };
+    fetchUsers();
   }, []);
 
-  const onSearchChange = (e) => {
+  const onSearchChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const searchedVal = e.target.value.toLowerCase();
     setSearchField(searchedVal);
   };
   const searchedMonster = monsters.filter((monster) => {
     return monster.name.toLowerCase().includes(searchField);
   });
+
   return (
-    <div className="App">
-      <h1 className="app-title">Monster Rolodex</h1>
+    <div className='App'>
+      <h1 className='app-title'>Monster Rolodex</h1>
       <SearchBox
         onChangeHandler={onSearchChange}
         placeholder={"Search Monster"}
         className={"searchbox"}
       />
-      <div className="card-list">
+      <div className='card-list'>
         <CardList monsters={searchedMonster} />;
       </div>
     </div>
